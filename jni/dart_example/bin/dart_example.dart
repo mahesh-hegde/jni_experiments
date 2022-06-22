e9a77c52e22ec836e30c503352205aca46579fb5
Mahesh Hegde <net.mahesh29@gmail.com>
2022-06-22 21:03:26 +0530

    improve example

--- jni/dart_example/bin/dart_example.dart
+++ jni/dart_example/bin/dart_example.dart
@@ -4,7 +4,8 @@ import 'dart:io';
 import 'package:path/path.dart';
 
 // This example shows how to use an external Java JAR using classpath
-// on dart standalone / flutter desktop.
+// on dart standalone / flutter desktop. This tool prints the text in PDF
+// file to standard output using PDFBox library.
 //
 // This is only meant to demonstrate the usage of package:jni's utility API
 // A better way to do this will be code generation, which is main part of this
@@ -15,28 +16,45 @@ import 'package:path/path.dart';
 // * Download PDFBox, FontBox, and commons logging JARs from APACHE site
 // * build libdartjni.so in src/ directory of package:jni, and move it to bin/
 
-String getText(String file) {
+void writeText(String file) {
   var jni = Jni.getInstance();
+
   // var pdfTextStripper = new PDFTextStripper()
   var pdfTextStripper =
       jni.newInstance("org/apache/pdfbox/text/PDFTextStripper", "()V", []);
+
   // var inputFile = new FileInputStream(file)
   var inputFile = jni
       .newInstance("java/io/FileInputStream", "(Ljava/lang/String;)V", [file]);
+
   // pdDoc = PDDocument.load(inputFile)
   var pdDoc = jni.invokeObjectMethod(
       "org/apache/pdfbox/pdmodel/PDDocument",
       "load",
-      "(Ljava/io/InputStream;)Lorg/apache/pdfbox/pdmodel/PDDocument;", [inputFile]);
-  // var text = pdfTextStripper.getText(pdDoc)
-  // Always pay close attention to signatures
-  var text = pdfTextStripper.callStringMethodByName("getText",
-      "(Lorg/apache/pdfbox/pdmodel/PDDocument;)Ljava/lang/String;", [pdDoc]);
+      "(Ljava/io/InputStream;)Lorg/apache/pdfbox/pdmodel/PDDocument;",
+      [inputFile]);
+
+  // var out = new OutputStreamWriter(System.out)
+  var out = jni.retrieveObjectField(
+      "java/lang/System", "out", "Ljava/io/PrintStream;");
+  var outWriter = jni.newInstance("java/io/OutputStreamWriter", "(Ljava/io/OutputStream;)V", [out]);
+
+  // pdfTextStripper.writeText(pdDoc, out)
+
+  // Always pay close attention to signatures. It's the most error prone part.
+  // If you get a method not found error, it's almost certainly related to signature.
+  pdfTextStripper.callVoidMethodByName(
+      "writeText",
+      "(Lorg/apache/pdfbox/pdmodel/PDDocument;Ljava/io/Writer;)V",
+      [pdDoc, outWriter]);
+
+  // out.close()
+  outWriter.callVoidMethodByName("close", "()V", []);
+
   // delete local objects
-  for (var i in [inputFile, pdDoc, pdfTextStripper]) {
-	  i.delete();
+  for (var i in [inputFile, outWriter, out, pdDoc, pdfTextStripper]) {
+    i.delete();
   }
-  return text;
 }
 
 void main(List<String> arguments) {
@@ -45,11 +63,14 @@ void main(List<String> arguments) {
   // download Apache pdfbox, fontbox and commons logging into jar/ folder using wget
   final pdfBoxJar = join(dirname(libPath), "jar", "pdfbox-2.0.26.jar");
   final fontBoxJar = join(dirname(libPath), "jar", "fontbox-2.0.26.jar");
-  final commonsLoggingJar = join(dirname(libPath), "jar", "commons-logging-1.2.jar");
+  final commonsLoggingJar =
+      join(dirname(libPath), "jar", "commons-logging-1.2.jar");
 
-  Jni.spawn(helperDir: libPath, classPath: [pdfBoxJar, fontBoxJar, commonsLoggingJar]);
+  Jni.spawn(
+      helperDir: libPath,
+      classPath: [pdfBoxJar, fontBoxJar, commonsLoggingJar]);
 
-  stdout.write(getText(arguments[0]));
+  writeText(arguments[0]);
 
   /*
   // Simple hello world.
